44aa5d876c93de87bc1e227e0c3914bc79594dcd
gs <vtqktvn123@gmail.com>
2025-10-19 19:46:15 +0700

    update blog home page & blog frontend

--- blog/views.py
+++ blog/views.py
@@ -4,8 +4,19 @@ from django.template import loader
 from .models import Blogs
 
 def index(request):
+    list_blogs = Blogs.objects.order_by("-pub_date")
+    date = [b.pub_date for b in list_blogs]
+
+    list_date_and_blogs = zip(list_blogs,date)
+
     template = loader.get_template("blogs_list.html")
-    return HttpResponse(template.render())
+
+
+    context = {
+        "list_date_and_blogs": list_date_and_blogs,
+    }
+    
+    return HttpResponse(template.render(context, request))
 
 def blog_view(request, blog_title):
     blog_template = loader.get_template("blog.html")
