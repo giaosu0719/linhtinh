47765ede2c989ca9e45555e36bace9d9ec09f95b
gs <vtqktvn123@gmail.com>
2025-10-19 16:05:51 +0700

    settings

--- app/settings_default.py
+++ app/settings_default.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = ['*']
 
 INSTALLED_APPS = [
     'shortlink.apps.ShortlinkConfig',
+    'blog.apps.BlogConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
